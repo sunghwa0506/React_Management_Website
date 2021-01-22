0cba849c11b3e080678692a886835918534ac513
sunghwa0506 <95sunghwa050604271013@gmail.com>
2021-01-22 14:18:32 -0600

    fetch data from backend

--- client/src/App.js
+++ client/src/App.js
@@ -5,71 +5,58 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-import {withSytle} from '@material-ui/core/styles';
+import React, { Component } from 'react';
 
+class App extends Component{
 
+  state = {customer:""}
 
-const customer = [
-  {
-    'id':1,
-    'image':'https://placeimg.com/64/64/1',
-    'name':'1',
-    'age':'25',
-    'school':'University of Texas at Arlington',
-    'gender':'Male'
-  },
-  {
-    'id':2,
-    'image':'https://placeimg.com/64/64/2',
-    'name':'2',
-    'age':'25',
-    'school':'University of Texas at Arlington',
-    'gender':'Male'
-  },
-  {
-    'id':3,
-    'image':'https://placeimg.com/64/64/3',
-    'name':'3',
-    'age':'25',
-    'school':'University of Texas at Arlington',
-    'gender':'Male'
+  componentDidMount(){
+    this.callApi()
+      .then(res =>this.setState({customer:res}))
+      .catch(err =>console.log(err));
   }
-]
+
+  callApi = async () =>{
+    const response = await fetch('/api/customer');
+    const body = await response.json();
+    return body;
+  }
+
+  render(){
 
 
+    return (
+      <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>id</TableCell>
+          <TableCell>image</TableCell>
+          <TableCell>age</TableCell>
+          <TableCell>gender</TableCell>
+          <TableCell>school</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {this.state.customer ? this.state.customer.map(c=>{
+          return(
+            <Customer
+            id = {c.id}
+            image = {c.image}
+            gender = {c.gender}
+            name = {c.name}
+            school = {c.school}f
+            age = {c.age}>
 
-function App() {
-  return (
-    
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>id</TableCell>
-              <TableCell>image</TableCell>
-              <TableCell>age</TableCell>
-              <TableCell>gender</TableCell>
-              <TableCell>school</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {customer.map(c=>{
-              return(
-                <Customer
-                id = {c.id}
-                image = {c.image}
-                gender = {c.gender}
-                name = {c.name}
-                school = {c.school}f
-                age = {c.age}>
-
-                </Customer>
-              );
-            })}
-          </TableBody>
-          
-        </Table>
-        
-  );
+            </Customer>
+          );
+        }) : "" }
+      </TableBody>
+      
+    </Table>
+    );
+  }
 }
+
 
 export default App;
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello',(req,res)=>{
-    res.send({message: 'hello express!!'});
+app.get('/api/customer',(req,res)=>{
+    res.send([
+        {
+          'id':1,
+          'image':'https://placeimg.com/64/64/1',
+          'name':'1',
+          'age':'25',
+          'school':'University of Texas at Arlington',
+          'gender':'Male'
+        },
+        {
+          'id':2,
+          'image':'https://placeimg.com/64/64/2',
+          'name':'2',
+          'age':'25',
+          'school':'University of Texas at Arlington',
+          'gender':'Male'
+        },
+        {
+          'id':3,
+          'image':'https://placeimg.com/64/64/3',
+          'name':'3',
+          'age':'25',
+          'school':'University of Texas at Arlington',
+          'gender':'Male'
+        }
+      ]);
 });
 
 app.listen(port,()=>console.log(`Listening on port ${port}`));

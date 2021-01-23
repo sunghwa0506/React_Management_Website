58e3c45c78c5617e78c9b7bd306eb1c89028acce
sunghwa0506 <95sunghwa050604271013@gmail.com>
2021-01-23 16:50:47 -0600

    connect the database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require ('mysql');
+
+
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password : conf.password,
+  port:      conf.port,
+  database:  conf.database
+});
+
+connection.connect((err)=>{
+  if(err){
+    console.log(err);
+  }
+});
+
+
 app.get('/api/customer',(req,res)=>{
-    res.send([
-        {
-          'id':1,
-          'image':'https://placeimg.com/64/64/1',
-          'name':'1',
-          'age':'25',
-          'school':'University of Texas at Arlington',
-          'gender':'Male'
-        },
-        {
-          'id':2,
-          'image':'https://placeimg.com/64/64/2',
-          'name':'2',
-          'age':'25',
-          'school':'University of Texas at Arlington',
-          'gender':'Male'
-        },
-        {
-          'id':3,
-          'image':'https://placeimg.com/64/64/3',
-          'name':'3',
-          'age':'25',
-          'school':'University of Texas at Arlington',
-          'gender':'Male'
-        }
-      ]);
+  connection.query("Select * from management",(err, rows, fields)=>{
+    res.send(rows);
+  }
+  );
 });
 
 app.listen(port,()=>console.log(`Listening on port ${port}`));
